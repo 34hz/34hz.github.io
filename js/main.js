20fc8cfdad2df59e57ea14283d7df5f26711ef7a
34hz <110100hz@gmail.com>
2020-12-26 05:48:05 +0800

    Site updated: 2020-12-26 05:48:05

--- js/main.js
+++ js/main.js
@@ -819,6 +819,10 @@ document.addEventListener('DOMContentLoaded', function () {
     switchComments()
   }
 
+  var full_page = document.getElementsByClassName("full_page");
+    if (full_page.length != 0) {
+    full_page[0].style.background = "transparent";
+}
   refreshFn()
   unRefreshFn()
 })
