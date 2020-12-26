c21d050b5f7b6f6c8480f43d7d0813560064a4eb
34hz <110100hz@gmail.com>
2020-12-27 07:18:10 +0800

    Site updated: 2020-12-27 07:18:10

--- js/mirror.js
+++ js/mirror.js
@@ -5,11 +5,16 @@ function Mirror() {
     pathname = window.location.pathname;
     hostname = window.location.hostname;
     // if (hostname === '域名，不带https://协议')
+    // if(pathname === '/'){
+    //     url=hostname;
+    //     window.location.href = url;
+    // }
     if (hostname === 'www.34hz.top') { //如果是主站
       // url = "域名，带https://协议" + pathname;
       url = "http://didibobo.gitee.io" + pathname; //就跳转到镜像站的同名页面
-      window.location.href = url;
       window.alert(pathname);
+      window.location.href = url;
+
     }
     else if(hostname === 'didibobo.gitee.io') {
       url = "http://www.34hz.top" + pathname;
